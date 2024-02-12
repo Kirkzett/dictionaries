16e513ce69dd1aab874e19867c43192cc659d422
Kirk <kirk_zetterstrom1@baylor.edu>
2024-02-12 14:09:35 -0600

    new csv

--- 5_datastore.py
+++ 5_datastore.py
@@ -47,3 +47,13 @@ datastore = { "medical":[
 
       ]
 }
+
+outfile = open('retial_space.csv','w')
+outfile.write(f'room-number,use,sq-ft,price\n')
+
+
+
+for d in datastore["medical"]:
+    outfile.write(f"{d['room-number']},{d['use']},{d['sq-ft']},{d['price']}\n")
+
+outfile.close()
