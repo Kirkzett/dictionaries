5dd95c80f853cc679a052ac6502e6a3dec0664a8
Kirk <kirk_zetterstrom1@baylor.edu>
2024-02-15 22:09:49 -0600

    rid

--- movie_report.py
+++ movie_report.py
@@ -28,15 +28,15 @@ def recommend_movie(movie_ratings, movie_title ):
 
     
     if movie_title in movie_ratings and movie_ratings[movie_title] >= 8:
-        print(f'We recomend {movie_title} with a rating of {movie_ratings[movie_title]} out of 10')
+        print(f'I recomend {movie_title} with a rating of {movie_ratings[movie_title]} out of 10')
     
     elif movie_title in movie_ratings and movie_ratings[movie_title] < 8:
-        print(f'We reccomend the following movies:')
+        print(f'I reccomend the following movies:')
         for movies, rating in movie_ratings.items():
             if rating >= 8:
                 print(f'{movies}, {rating} out of 10')
     else:
-        print(f'Your movie was not found, here are some movies we reccomend:')
+        print(f'Your movie was not found, here are some movies I reccomend:')
         for movies, rating in movie_ratings.items():
             if rating >= 8:
                 print(f'{movies}, {rating} out of 10')
